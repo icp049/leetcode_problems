97999c0cbad0830c7255a7b7b7ec2d883fa81289
icp049 <108185265+icp049@users.noreply.github.com>
2023-05-30 00:05:19 -0600

    Add files via upload

---
+++ PhoneCombination.cpp
@@ -0,0 +1,32 @@
+class Solution{
+    private:
+
+    map<char, string> m = {
+        {'2', "abc"}, {'3', "def"}, {'4',"ghi"},
+        {'5',"jkl"},{'6',"mno"},{'7',"pqrs"},
+        {'8', "tuv"}, {'9', "wxyz"}
+    };
+
+    vector<string> result;
+
+
+    public:
+    vector<string> letterCombinations(string digits){
+        if(digits.size()!= 0){
+            generateCombination("",digits,0);
+        }
+        return result;
+    }
+
+    void generateCombination(string current, string digits, int index){
+        if(index == digits.size()){
+            result.push_back(current);
+        } else {
+            char currentDigit = digits[index];
+            string mapping = m[currentDigit];
+            for(int i = 0; i < mapping.size(); i++){
+                generateCombination(current + mapping[i], digits, index+1);
+            }
+        }
+    }
+};
